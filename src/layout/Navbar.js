4c4516afe9ec53769d2e55e84d2a275344d5f7f0
jafet <jafetpintle@gmail.com>
2023-04-23 19:30:33 -0600

    Add button add to navbar

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -3,9 +3,9 @@ import React from 'react'
 export default function Navbar() {
   return (
     <div>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <div className="container-fluid">
-            <a className="navbar-brand" href="#">Navbar</a>
+            <a className="navbar-brand" href="#">Todo List</a>
             <button className="navbar-toggler" 
                 type="button" 
                 data-bs-toggle="collapse" 
@@ -14,6 +14,9 @@ export default function Navbar() {
                 aria-expanded="false" 
                 aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
+            </button>
+            
+            <button className="btn btn-outline-light">Add user
             </button>
         </div>
         </nav> 
